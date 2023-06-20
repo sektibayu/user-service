58ee9c38862f7bdc598fa98722c2c4ac6ff49ac0
Bayu Sektiaji <bayu.sektiaji@kuncie.com>
2023-06-20 18:30:23 +0700

    add: unit testing

--- go.mod
+++ go.mod
@@ -9,12 +9,14 @@ require (
 	github.com/golang/mock v1.6.0
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.8.3
 )
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
@@ -39,6 +41,7 @@ require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/perimeterx/marshmallow v1.1.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- handler/endpoints_test.go
+++ handler/endpoints_test.go
@@ -1,7 +1,109 @@
-package handler
+package handler_test
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/SawitProRecruitment/UserService/generated"
+	"github.com/SawitProRecruitment/UserService/handler"
+	"github.com/SawitProRecruitment/UserService/repository"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRegistration(t *testing.T) {
+	// setup mock
+	ctrl := gomock.NewController(t)
+	repo := repository.NewMockRepositoryInterface(ctrl)
+	h := handler.NewServer(handler.NewServerOptions{
+		Repository: repo,
+	})
 
+	var testcases = []struct {
+		name        string
+		params 		*generated.RegisterRequest
+		mockFunc  func (ctx context.Context)
+		expect      interface{}
+	}{
+		{
+			name: "when no params",
+			params: &generated.RegisterRequest{
+				FullName: "",
+				PhoneNumber: "",
+				Password: "",
+			},
+			mockFunc: func(ctx context.Context) {},
+			expect: &generated.ErrorResponse{
+				Message: "Bad Request",
+				Errors: []generated.ErrorField{
+					{ Field: "phone_number", Message: "phone_number must be at minimum 10 char and maximum 13 char" },
+					{ Field: "full_name", Message: "full_name must be at minimum 3 char and maximum 60 char" },
+					{ Field: "password", Message: "password must be at minimum 6 char and maximum 64 char" },
+				},
+			},
+		},
+		{
+			name: "when phone number not using indonesia code ",
+			params: &generated.RegisterRequest{
+				FullName: "bayu sektiaji",
+				PhoneNumber: "+61899999999",
+				Password: "Kupu=Kupu4",
+			},
+			mockFunc: func(ctx context.Context) {},
+			expect: &generated.ErrorResponse{
+				Message: "Bad Request",
+				Errors: []generated.ErrorField{
+					{ Field: "phone_number", Message: "phone_number must start indonesia country code: +62" },
+				},
+			},
+		},
+		{
+			name: "when success",
+			params: &generated.RegisterRequest{
+				FullName: "bayu sektiaji",
+				PhoneNumber: "+62899999999",
+				Password: "Kupu=Kupu4",
+			},
+			mockFunc: func(ctx context.Context) {
+				repo.EXPECT().CreateNewUser(gomock.Any(), gomock.Any()).Return(repository.User{
+					Id: "asdf-adsf-adf-afd",
+					FullName: "bayu sektiaji",
+					PhoneNumber: "+62899999999",
+					HashPassword: "lalalili",
+					Salt: "lalalili",
+				}, nil)
+			},
+			expect: &generated.RegisterResponse{
+				Id: "asdf-adsf-adf-afd",
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			body, _ := json.Marshal(tc.params)
+			req := httptest.NewRequest(http.MethodPost, "/registration", strings.NewReader(string(body)))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			ctx := echo.New().NewContext(req, rec)
+			tc.mockFunc(ctx.Request().Context())
+			h.Registration(ctx)
+			var result interface{}
+			_, isError := tc.expect.(*generated.ErrorResponse)
+			if isError {
+				result = &generated.ErrorResponse{}
+			} else {
+				result = &generated.RegisterResponse{}
+			}
+			json.Unmarshal(rec.Body.Bytes(), result)
+			rec.Body.Reset()
+
+			assert.Equal(t, tc.expect, result)
+		})
+	}
 }
